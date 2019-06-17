13fddb1a5aec219b5755346b0c17b694face68c9
Kyle Oliveri <KyleOliveri@Kyles-Mac-Pro.local>
2019-06-16 19:40:07 -0700

    Initial File Upload / Created Header Component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment, useEffect } from 'react';
+import Header from "./components/layout/Header";
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    //Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./assets/css/Header.css"
+
+const Navbar = () => {
+    return (
+        <div id="backgroundImg">
+
+            {/* Navbar */}
+            <nav className="transparent no-shadows">
+                <div className="nav-wrapper">
+                    <a href="#" data-target="mobile-demo" className="sidenav-trigger black-text"><i className="material-icons">menu</i></a>
+                    <ul className="right hide-on-med-and-down">
+                        <li><a href="#" className="black-text">Home</a></li>
+                        <li><a href="#" className="black-text">Projects</a></li>
+                        <li><a href="#" className="black-text">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <ul className="sidenav" id="mobile-demo">
+                <li><a href="#" className="">Home</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+
+
+
+        </div>
+    )
+}
+
+export default Navbar
